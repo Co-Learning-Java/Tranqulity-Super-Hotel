08bc5a29f98751240aad61499c2589efd4c6fc8f
Decagon <decagon@Decagons-MacBook-Air.local>
2022-06-05 16:46:11 +0100

    Setting up swagger configuration

--- src/main/java/com/colearningjava/tranquilitysuperhotel/TranquilitySuperHotelApplication.java
+++ src/main/java/com/colearningjava/tranquilitysuperhotel/TranquilitySuperHotelApplication.java
@@ -3,7 +3,6 @@ package com.colearningjava.tranquilitysuperhotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class TranquilitySuperHotelApplication {
 
     public static void main(String[] args) {
