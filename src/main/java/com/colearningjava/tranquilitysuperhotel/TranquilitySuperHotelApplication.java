b5e96aa38926f69a2b7a56603503b730c1b97172
Decagon <decagon@Decagons-MacBook-Air.local>
2022-06-05 16:31:37 +0100

    Creating Project Boilerplate

---
+++ src/main/java/com/colearningjava/tranquilitysuperhotel/TranquilitySuperHotelApplication.java
@@ -0,0 +1,13 @@
+package com.colearningjava.tranquilitysuperhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranquilitySuperHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranquilitySuperHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colearningjava/tranquilitysuperhotel/TranquilitySuperHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colearningjava.tranquilitysuperhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranquilitySuperHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
